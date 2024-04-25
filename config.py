c6cf8a16238db12cfa1bbc1b179a0fae7f0c1842
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-04-26 01:28:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -43,7 +43,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AlmortageLL/Alm",
+    "https://github.com/MOHAMEDELMORT/Alm",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(

f991c7ff8423c42883ee3950e2c1763ded63e0d6
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-04-25 22:38:03 +0200

    Add files via upload

---
+++ Almortagel/__init__.py
@@ -0,0 +1,26 @@
+from Almortagel.core.bot import Dil
+from Almortagel.core.dir import dirr
+from Almortagel.core.git import git
+from Almortagel.core.userbot import Userbot
+from Almortagel.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Dil()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()

7f86681715f3600d74150c1a0a23c0a3125b14f9
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-04-27 02:18:11 +0300

    Update Gay.py

--- Almortagel/plugins/Management/Gay.py
+++ Almortagel/plugins/Management/Gay.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import random
-from AarohiX import app
+from Almortagel import app
 
 def calculate_gay_percentage():
     return random.randint(1, 100)

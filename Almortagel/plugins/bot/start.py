4f910625762eb6fa1ce4fd85c659823e20af2f4e
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-04-27 02:44:07 +0300

    Update start.py

--- Almortagel/plugins/bot/start.py
+++ Almortagel/plugins/bot/start.py
@@ -103,12 +103,12 @@ async def start_gp(client, message: Message, _):
     out = start_panel(_)
     uptime = int(time.time() - _boot_)
     await message.reply_photo(
-        im=im
         photo=config.START_IMG_URL,
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
     )
     return await add_served_chat(message.chat.id)
+
 
 @app.on_message(filters.new_chat_members, group=-1)
 async def welcome(client, message: Message):

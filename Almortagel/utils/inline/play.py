316e8e922fff314ca071904791421afa4643cd5a
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-05-06 08:44:33 +0300

    Update play.py

--- Almortagel/utils/inline/play.py
+++ Almortagel/utils/inline/play.py
@@ -58,7 +58,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
         [
-            InlineKeyboardButton(text=f"{OWNER_NAME}", url=f"tg://openmessage?user_id={OWNER_ID}",
+            InlineKeyboardButton(text="DEV ELMORTAGEL", url=f"https://t.me/ALMORTAGEL_12",
         )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -82,7 +82,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
         [
-            InlineKeyboardButton(text=f"{OWNER_NAME}", url=f"tg://openmessage?user_id={OWNER_ID}",
+            InlineKeyboardButton(text="DEV ELMORTAGEL", url=f"https://t.me/ALMORTAGEL_12",
         )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],

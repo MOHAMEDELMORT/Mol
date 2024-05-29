fe2fe66f38615f088d8cc507fc2c0a143249e1d2
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-05-29 17:25:27 +0300

    Update الإشتراك الاجباري.py

--- Almortagel/plugins/play/الإشتراك الاجباري.py
+++ Almortagel/plugins/play/الإشتراك الاجباري.py
@@ -6,7 +6,7 @@ from config import Almortagel
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
-    if not Muntazer:
+    if not Almortagel:
         return
     try:
         try:

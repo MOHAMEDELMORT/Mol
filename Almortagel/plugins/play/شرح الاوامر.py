00f4513441896592cae0b95f770d115a1c5f6ecc
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-06-06 00:33:39 +0300

    Update شرح الاوامر.py

---
+++ Almortagel/plugins/play/شرح الاوامر.py
@@ -0,0 +1,16 @@
+from OpsAi import Ai
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from Almortagel import app
+
+
+
+
+@app.on_message(filters.regex(r"^(المرتجل)"))
+async def aiText(_, message: Message):
+    data = message.text.split(maxsplit=1)
+    if len(data) < 2: return await message.reply("- عايز اي؟", reply_to_message_id=message.id)
+    wait = await message.reply("- استنى بكتب..✍️", reply_to_message_id=message.id)
+    query = data[1]
+    response = Ai(query=query)
+    return await wait.edit_text(response.chat())

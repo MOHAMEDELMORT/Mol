bb25a047f3ec8f25cfa71957165f728233ada298
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-06-07 00:52:52 +0300

    Update المطور.py

--- Almortagel/plugins/play/المطور.py
+++ Almortagel/plugins/play/المطور.py
@@ -1,28 +1,25 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from strings.filters import command
+from config import OWNER_NAME
 from Almortagel import app
 import config
 
 @app.on_message(
-    command(["مطور البوت"])
+    command(["المطور"])
 )
-async def maker(client: Client, message: Message):
-    await message.reply_text(text="""مرحبا بكمرحبا بك عزيزي {message.from_user.mention} \nللتحدث مع مطور البوت اضغط علي الازرار بالاسفل👇\n**⩹━★⊷━⌞ ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ ⌝━⊶★━⩺**""",
+async def huhh(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/14c7948ad180050fe16e4.jpg",
+        caption=f"""**⩹━★⊷━⌞ ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ ⌝━⊶★━⩺**\nمرحبا بك عزيزي {message.from_user.mention} في قسم المطور\nللتحدث مع مطور البوت اضغط علي الازرار بالاسفل👇\n**⩹━★⊷━⌞ ѕᴏụʀᴄᴇ ᴀʟᴍᴏʀᴛᴀɢᴇʟ ⌝━⊶★━⩺**""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "- مطور البوت .", url=f"tg://openmessage?user_id={config.OWNER_ID}"
-                    ),
+                   InlineKeyboardButton(text=f"{OWNER_NAME}", user_id=config.OWNER_ID),
                 ],
-                [
-                    InlineKeyboardButton(
-                        "- قناة البوت . ", url=config.SUPPORT_CHAT
-                    ),
-                ],
+
             ]
+
         ),
+
     )
-
-

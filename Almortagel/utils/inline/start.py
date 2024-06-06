6d32934b6625cdc461ccb22539c77a82ebc3d1b5
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-06-06 21:01:19 +0300

    Update start.py

--- Almortagel/utils/inline/start.py
+++ Almortagel/utils/inline/start.py
@@ -20,18 +20,17 @@ def private_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
+                text=_["S_B_8"],
+                callback_data=f"arbic",
+            ),
+            InlineKeyboardButton(
+                text=_["S_B_6"],
+                callback_data=f"english",
+            ),
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_10"], url=f"https://t.me/ALMORTAGEL_12"),
+                    InlineKeyboardButton(text=_["S_B_10"], url=f"https://t.me/ALMORTAGEL_12"),
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
     ]
     return buttons

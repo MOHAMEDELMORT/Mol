02c4c90416318363a0c1eca770b4e5ecc550de95
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-06-15 16:52:44 +0300

    Update play.py

--- Almortagel/utils/inline/play.py
+++ Almortagel/utils/inline/play.py
@@ -1,6 +1,6 @@
 import math
 import config
-from config import SUPPORT_CHAT, OWNER_ID, OWNER_NAME
+from config import SUPPORT_CHAT, OWNER_NAME, OWNER_ID 
 from pyrogram.types import InlineKeyboardButton
 
 from Almortagel.utils.formatters import time_to_seconds
@@ -58,7 +58,7 @@ def stream_markup_timer(_, chat_id, played, dur):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
         [
-            InlineKeyboardButton(text=f"{OWNER_NAME}", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=f"{OWNER_NAME}", user_id=config.OWNER_ID)
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -81,7 +81,7 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT)
         ],
         [
-            InlineKeyboardButton(text=f"{OWNER_NAME}", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=f"{OWNER_NAME}", user_id=config.OWNER_ID)
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]

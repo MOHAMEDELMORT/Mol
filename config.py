e376678830fa6448185c74f6296c25dadfe732e4
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-06-20 20:04:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "المرتجل")
 
 BOT_ID = getenv("BOT_ID", "Almortagel_music_bot")
 
-MURTAGEL = getenv("MURTAGEL", "AlmortagelTech")
+MURTAGEL = getenv("MURTAGEL", None)
 
 OWNER_NAME = getenv("OWNER_NAME", "DEVALMORTAGEL")
 

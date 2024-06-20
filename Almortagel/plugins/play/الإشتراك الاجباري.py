d1e71cef0321f53ea8681617fafcc40a9a467c08
MOHAMEDELMORT <135501682+MOHAMEDELMORT@users.noreply.github.com>
2024-06-20 19:52:48 +0300

    Update الإشتراك الاجباري.py

--- Almortagel/plugins/play/الإشتراك الاجباري.py
+++ Almortagel/plugins/play/الإشتراك الاجباري.py
@@ -1,33 +1,36 @@
+from config import MURTAGEL
+
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from Almortagel import app
-import config
-from config import Mortagel
 
-@app.on_message(filters.incoming & filters.private, group=-1)
-async def must_join_channel(app: Client, msg: Message):
-    if not Mortagel:
+
+@Client.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not MURTAGEL:
         return
     try:
         try:
-            await app.get_chat_member(Mortagel, msg.from_user.id)
+            await bot.get_chat_member(MURTAGEL, msg.from_user.id)
         except UserNotParticipant:
-            if Mortagel.isalpha():
-                link = "https://t.me/" + Mortagel
+            if MUST_JOIN.isalpha():
+                link = "https://t.me/" + MURTAGEL
             else:
-                chat_info = await app.get_chat(Mortagel)
+                chat_info = await bot.get_chat(MURTAGEL)
                 link = chat_info.invite_link
             try:
-                await msg.reply(
-                    f"~︙عزيزي {msg.from_user.mention} \n~︙عليك الأشتراك في قناة البوت \n~︙قناة البوت : @{Mortagel}.",
-                    disable_web_page_preview=True,
-                    reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("< قناة البوت >", url=link)]
-                    ])
+                await msg.reply_photo(
+                    photo="https://telegra.ph/file/6b5f18c07143327344dd2.jpg", caption=f"» 📣 لا يمكنك استخدام البوت . [ٍAlmortagelTech 𖠮({link}) 🔘 الا بعد الاشتراك بقناة البوت . [AlmortagelTech 𖠮]({link}) 📡 اشترك بقناة بعدها ارسل /start .",
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton("AlmortagelTech 𖠮", url=link),
+                            ]
+                        ]
+                    )
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I m not admin in the MUST_JOIN chat {Mortagel}!")
+        print(f"Promote me as an admin in the MURTAGEL chat : {MURTAGEL} !")
